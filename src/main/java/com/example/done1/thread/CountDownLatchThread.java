92dcf1c3fef289aa94a16264da12f0137eaee79d
陈艳军 <chenyanjun@guide-soft.com>
2022-05-09 18:38:13 +0800

    同步线程在执行主线程

--- src/main/java/com/example/done1/thread/CountDownLatchThread.java
+++ src/main/java/com/example/done1/thread/CountDownLatchThread.java
@@ -4,6 +4,8 @@ import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.RandomUtil;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Admin
@@ -40,21 +42,55 @@ public class CountDownLatchThread {
             //初始化线程数量
             //新建一个CountDownLatch，一个同步辅助类，在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待。
             CountDownLatch countDownLatch=ThreadUtil.newCountDownLatch(5);
-            for(int i=0;i<5;i++){
-                ThreadUtil.execute(() -> {
-                    int num = RandomUtil.randomInt(1000, 4000);
-                    System.out.println(num);
-                    ThreadUtil.sleep(1000);
-                    //调用线程计数器-1
-                    countDownLatch.countDown();
-                });
-                System.out.println("第"+ i +"个线程" );
+            AtomicInteger a = new AtomicInteger();
+            AtomicInteger b = new AtomicInteger();
+            AtomicInteger c = new AtomicInteger();
+            AtomicInteger d = new AtomicInteger();
+            AtomicInteger e = new AtomicInteger();
+            System.out.println("1");
+            ThreadUtil.execute(() -> {
+                ThreadUtil.sleep(3000);
+                a.set(1);
+                System.out.println(a.get()+"a");
                 //调用线程计数器-1
-//                countDownLatch.countDown();
-            }
+                countDownLatch.countDown();
+            });
+            System.out.println("2");
+            ThreadUtil.execute(() -> {
+                ThreadUtil.sleep(3000);
+                b.set(0);
+                System.out.println(b.get()+"b");
+                //调用线程计数器-1
+                countDownLatch.countDown();
+            });
+            System.out.println("3");
+            ThreadUtil.execute(() -> {
+                ThreadUtil.sleep(3000);
+                c.set(1);
+                System.out.println(c.get()+"c");
+                //调用线程计数器-1
+                countDownLatch.countDown();
+            });
+            System.out.println("4");
+            ThreadUtil.execute(() -> {
+                ThreadUtil.sleep(3000);
+                d.set(1);
+                System.out.println(d.get()+"d");
+                //调用线程计数器-1
+                countDownLatch.countDown();
+            });
+            System.out.println("5");
+            ThreadUtil.execute(() -> {
+                ThreadUtil.sleep(3000);
+                e.set(1);
+                System.out.println(e.get()+"e");
+                //调用线程计数器-1
+                countDownLatch.countDown();
+            });
+            System.out.println("6");
             //唤醒主线程
             countDownLatch.await();
-            ThreadUtil.sleep(3000);
+//            ThreadUtil.sleep(3000);
             System.out.println("完成");
         } catch (Exception e) {
             e.printStackTrace();
